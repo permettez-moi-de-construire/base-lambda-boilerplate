f84abf35e59b660cb58cab87791d4221a885a793
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2019-02-19 09:34:32 +0100

    Add test boilerplate

---
+++ hskoumoul-service/tests/pouet.test.js
@@ -0,0 +1,16 @@
+const {
+  describe,
+  it
+} = require('mocha')
+
+const assert = require('assert')
+
+describe('MyPouet class', () => {
+  it('My first test runs', () => {
+
+  })
+
+  it('My second test fails', () => {
+    
+  })
+})
